0a7211ab21c1624815fb4ee2214f7d685b6bcbdf
EbaraKoji <kojieva5270@gmail.com>
2024-03-08 22:05:33 +0900

    revise templates for gemini agent with search tools

--- examples/serve/packages/gemini-functions-agent/gemini_functions_agent/agent.py
+++ examples/serve/packages/gemini-functions-agent/gemini_functions_agent/agent.py
@@ -1,41 +1,13 @@
-
 from typing import List, Tuple
 
-from langchain.agents import AgentExecutor
-from langchain.agents.format_scratchpad import format_to_openai_function_messages
-from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
-from langchain.tools import Tool, DuckDuckGoSearchResults  # noqa F401
-# from langchain.utilities.tavily_search import TavilySearchAPIWrapper
-# from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain.prompts import ChatPromptTemplate
+from langchain.tools import DuckDuckGoSearchResults
 from langchain_core.messages import AIMessage, HumanMessage
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-# Create the tool
-# search = TavilySearchAPIWrapper()
-# description = """"A search engine optimized for comprehensive, accurate, \
-# and trusted results. Useful for when you need to answer questions \
-# about current events or about recent information. \
-# Input should be a search query. \
-# If the user is asking about something that you don't know about, \
-# you should probably use this tool to see if that can provide any information."""
-# tavily_tool = TavilySearchResults(api_wrapper=search, description=description)
-
-# tools = [tavily_tool]
-
-duckduckgo_tool = DuckDuckGoSearchResults()
-tools = [duckduckgo_tool]
-
-llm = ChatGoogleGenerativeAI(temperature=0, model="gemini-pro")
-
-prompt = ChatPromptTemplate.from_messages([
-    MessagesPlaceholder(variable_name="chat_history"),
-    ("user", "{input}"),
-    MessagesPlaceholder(variable_name="agent_scratchpad"),
-])
-
-llm_with_tools = llm.bind(functions=tools)
+from .chat_template import react_chat_gemini_template
 
 
 def _format_chat_history(chat_history: List[Tuple[str, str]]):
@@ -44,16 +16,6 @@ def _format_chat_history(chat_history: List[Tuple[str, str]]):
         buffer.append(HumanMessage(content=human))
         buffer.append(AIMessage(content=ai))
     return buffer
-
-
-agent = ({
-    "input": lambda x: x["input"],
-    "chat_history": lambda x: _format_chat_history(x["chat_history"]),
-    "agent_scratchpad": lambda x: format_to_openai_function_messages(x["intermediate_steps"]),
-}
-         | prompt
-         | llm_with_tools
-         | OpenAIFunctionsAgentOutputParser())
 
 
 class AgentInput(BaseModel):
@@ -66,5 +28,12 @@ class AgentInput(BaseModel):
                                                "output": "output"
                                            }})
 
+
+duckduckgo_tool = DuckDuckGoSearchResults()
+tools = [duckduckgo_tool]
+
+llm = ChatGoogleGenerativeAI(temperature=0, model="gemini-pro")
+prompt = ChatPromptTemplate.from_template(react_chat_gemini_template)
+agent = create_react_agent(llm, tools, prompt)
 
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True).with_types(input_type=AgentInput)
